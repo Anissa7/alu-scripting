02db759d170da6691e1ef142f478d27eb92d0e43
Anissa7 <a.ouedraogo@alustudent.com>
2023-11-30 23:32:36 +0000

    the file is now working .

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,49 +1,52 @@
 #!/usr/bin/python3
-""""Doc"""
-import requests
+"""
+module for function to parses the title of all hot articles, and prints
+a sorted count of given keywords (case-insensitive, delimited by spaces
+"""
 
-def count_words(subreddit, word_list, after="", words_count={}):
-    url = "https://www.reddit.com/r/{}/hot.json?limit=100".format(subreddit)
-    header = {'User-Agent': 'Mozilla/5.0'}
-    param = {'after': after}
-    res = requests.get(url, headers=header, params=param)
 
-    if res.status_code != 200:
-        return
+def count_words(subreddit, word_list, instances={}, count=0, after=None):
+    """
+    Args:
+        subreddit: subreddit name
+        word_list: a list containing the keywords of all hot articles titles
+        instances: a dictionary containing the keywords and their frequency
+        count: counter to change prototype
+        after: to validate subreddit
 
-    json_res = res.json()
-    after = json_res.get('data').get('after')
-    has_next = after is not None
+    Returns:
+        returns a dictionary containing the frequency of certain
+        keywords in the hot articles
+    """
+    import requests
 
-    if len(words_count) == 0:
-        words_count = {word.lower(): 0 for word in word_list}
-
-    hot_articles = json_res.get('data').get('children')
-
-    for article in hot_articles:
-        title_words = article.get('data').get('title').lower().split()
-        for title_word in title_words:
+    headers = {'User-Agent': 'User-Agent'}
+    parameters = {'after': after,
+                  'count': count}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers=headers, params=parameters,
+                            allow_redirects=False)
+    if response.status_code == 200:
+        results = response.json().get("data")
+        after = results.get("after")
+        count += results.get("dist")
+        for child in results.get("children"):
+            hot_list = child.get("data").get("title").lower().split()
             for word in word_list:
-                if word.lower() == title_word:
-                    words_count[word.lower()] += 1
+                if word.lower() in hot_list:
+                    times = len([article for article in hot_list if
+                                article == word.lower()])
+                    if instances.get(word.lower()) is None:
+                        instances[word.lower()] = times
+                    else:
+                        instances[word.lower()] += times
 
-    if has_next:
-        return count_words(subreddit, word_list, after, words_count)
-    else:
-        words_count = {key: value for key, value in words_count.items() if value != 0}
-        sorted_counts = sorted(words_count.items(), key=lambda item: item[1], reverse=True)
-
-        for word, count in sorted_counts:
-            print("{}: {}".format(word, count))
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 3:
-        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
-    else:
-        subreddit = sys.argv[1]
-        word_list = sys.argv[2].split()
-        count_words(subreddit, word_list)
-
+        if after is None:
+            if len(instances) == 0:
+                return
+            instances = sorted(instances.items(),
+                               key=lambda kv: (-kv[1], kv[0]))
+            [print("{}: {}".format(k, v)) for k, v in instances]
+        else:
+            count_words(subreddit, word_list, instances, count, after)
+    return
